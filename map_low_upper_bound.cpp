81d0dc91ae2b7353157ed23d864d38be01e7c484
neil xavier dsouza <neil.xavier.dsouza@gmail.com>
2019-04-28 23:40:32 +0530

    understand better how map.lower_bound/upper_bound work

--- map_low_upper_bound.cpp
+++ map_low_upper_bound.cpp
@@ -6,38 +6,47 @@ using namespace std;
 
 int main ()
 {
-  std::map<char,int> mymap;
-  std::map<char,int>::iterator itlow,itup;
+  std::map<int,int> mymap;
+  std::map<int,int>::iterator itlow,itup;
 
-  mymap['a']=20;
+  mymap[1]=11;
   //mymap['b']=40;
-  mymap['c']=60;
+  mymap[2]=12;
   //mymap['d']=80;
-  mymap['e']=100;
+  mymap[3]=13;
+  mymap[7]=17;
+  mymap[8]=18;
 
-  itlow=mymap.lower_bound ('b');  // itlow points to b
-  itup=mymap.upper_bound ('b');   // itup points to e (not d!)
+  itlow=mymap.lower_bound (4);  // itlow points to b
+  itup=mymap.upper_bound (4);   // itup points to e (not d!)
+  cout << "itlow: " << itlow->first << "," << itlow->second << endl;
+  cout << "itup: " << itup->first << "," << itup->second << endl;
 
+
+  itlow=mymap.lower_bound (3);  // itlow points to b
+  itup=mymap.upper_bound (3);   // itup points to e (not d!)
+  cout << "itlow: " << itlow->first << "," << itlow->second << endl;
+  cout << "itup: " << itup->first << "," << itup->second << endl;
   
-  std::map<char,int>::iterator it1 = mymap.begin();
-  it1--;
-  if (it1 != mymap.begin()) {
-	  cout << " moved before begin" << endl;
-	  //cout << it1->first << it1->second << endl;
-	  if (it1 == mymap.end()) {
-		  cout << "it's map.end()" << endl;
-	  }
-  } else {
-	  cout << " still begin" << endl;
-  }
+  ///std::map<char,int>::iterator it1 = mymap.begin();
+  ///it1--;
+  ///if (it1 != mymap.begin()) {
+  ///        cout << " moved before begin" << endl;
+  ///        //cout << it1->first << it1->second << endl;
+  ///        if (it1 == mymap.end()) {
+  ///      	  cout << "it's map.end()" << endl;
+  ///        }
+  ///} else {
+  ///        cout << " still begin" << endl;
+  ///}
 
-  //mymap.erase(itlow,itup);        // erases [itlow,itup)
+  /////mymap.erase(itlow,itup);        // erases [itlow,itup)
 
-  // print content:
-  //for (std::map<char,int>::iterator it=mymap.begin(); it!=mymap.end(); ++it)
-  //  std::cout << it->first << " => " << it->second << '\n';
-  cout << itlow->first << endl;
-  cout << itup->first << endl;
+  ///// print content:
+  /////for (std::map<char,int>::iterator it=mymap.begin(); it!=mymap.end(); ++it)
+  /////  std::cout << it->first << " => " << it->second << '\n';
+  ///cout << itlow->first << endl;
+  ///cout << itup->first << endl;
 
   return 0;
 }
